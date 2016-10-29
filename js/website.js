4c7b7dcc8f42722413546bfd5e0089eb6fd00321
Saul Giordani <saulgiordani@MBP-di-Saul.fritz.box>
2016-10-29 19:45:01 +0200

    first commit

--- js/website.js
+++ js/website.js
@@ -1,43 +1,87 @@
-angular.module('website', ['ngRoute']).
-    config(function ($routeProvider) {
+(function () {
+'use strict';
+
+angular.module('website', ['ngRoute'])
+    .controller('ProdottiCtrl', ProdottiCtrl)
+    .controller('ChisiamoCtrl', ChisiamoCtrl)
+    .controller('HomeCtrl', HomeCtrl)
+    .controller('FooterCtrl', FooterCtrl)
+    .service('HomeService', HomeService)
+    .service('ChisiamoService', ChisiamoService)
+    .service('ProdottiService', ProdottiService)
+    .service('StateService', StateService)
+    .service('FooterService', FooterService)
+    .config(function ($routeProvider) {
         $routeProvider.
-            when('/about', {templateUrl: 'partials/about.html', controller: 'AboutCtrl'}).
-            when('/experiments', {templateUrl: 'partials/experiments.html', controller: 'ExperimentsCtrl'}).
+            when('/prodotti', {templateUrl: 'partials/prodotti.html', controller: 'ProdottiCtrl'}).
+            when('/chisiamo', {templateUrl: 'partials/chisiamo.html', controller: 'ChisiamoCtrl'}).
             when('/home', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'}).
             otherwise({redirectTo: '/home'});
     })
-    .controller('AboutCtrl', function ($scope, StateService) {
-        $scope.title = 'About Page';
-        $scope.body = 'This is the about page body';
+    .directive('experiment', ExperimentDirective);
+
+    function ExperimentDirective() {
+        var linker = function (scope, element, attrs) {
+            element.on('click', function(){
+                scope.doExperiment();
+            })
+        };
+
+        var controller =  function($scope) {
+            $scope.doExperiment = function() {
+                $scope.$apply(function(){
+                    $scope.experiment.completed++;
+                });
+            };
+        };
+
+        return {
+            scope: true,
+            restrict: 'E',
+            templateUrl: 'directives/productDetail.html',
+            link: linker,
+            controller: controller
+        }
+    }
+
+    HomeCtrl.$inject = ['HomeService'];
+    function HomeCtrl(HomeService) {
+        var listaNotizie = this;
+
+        listaNotizie.notizie = HomeService.getNotizie();
+    }
+
+    ProdottiCtrl.$inject = ['ProdottiService'];
+    function ProdottiCtrl(ProdottiService) {
+        var listaProdotti = this;
+
+        listaProdotti.prodotti = ProdottiService.getProdotti();
+    }
+
+    ChisiamoCtrl.$inject = ['$scope', 'StateService', 'ChisiamoService'];
+    function ChisiamoCtrl($scope, StateService, ChisiamoService) {
+        $scope.title = "L'Olio è la nostra vita";
+        $scope.body = "Informazione sull'azienda";
+
+        console.log(StateService);
+        console.log(ChisiamoService);
 
         $scope.message = StateService.getMessage();
+        $scope.experiments = ChisiamoService.getExperiments();
 
         $scope.updateMessage = function (m) {
             StateService.setMessage(m);
         };
-    })
-    .controller('ExperimentsCtrl', function ($scope, StateService, ExperimentsService) {
-        $scope.title = 'Experiments Page';
-        $scope.body = 'This is the about experiments body';
+    }
 
-        $scope.message = StateService.getMessage();
-        $scope.experiments = ExperimentsService.getExperiments();
+    FooterCtrl.$inject = ['FooterService'];
+    function FooterCtrl(FooterService) {
+        var footerList = this;
 
-        $scope.updateMessage = function (m) {
-            StateService.setMessage(m);
-        };
-    })
-    .controller('HomeCtrl', function ($scope, StateService) {
-        $scope.title = 'Home Page';
-        $scope.body = 'This is the about home body';
+        footerList.footers = FooterService.getFooters();
+    }
 
-        $scope.message = StateService.getMessage();
-
-        $scope.updateMessage = function (m) {
-            StateService.setMessage(m);
-        };
-    })
-    .factory('StateService', function () {
+    function StateService() {
         var message = 'Hello Message';
         var getMessage = function () {
             return message;
@@ -50,44 +94,60 @@ angular.module('website', ['ngRoute']).
             getMessage: getMessage,
             setMessage: setMessage
         }
-    })
-    .service('ExperimentsService', function () {
-        var service = this,
-            experiments = [
-                {name: 'Experiment 1', description: 'This is an experiment', completed:0},
-                {name: 'Experiment 2', description: 'This is an experiment', completed:0},
-                {name: 'Experiment 3', description: 'This is an experiment', completed:0},
-                {name: 'Experiment 4', description: 'This is an experiment', completed:0}
-            ];
+    }
+
+    function HomeService() {
+        var service = this;
+        var notizie = [
+            { titolo: "Notizia più recente", testo: "Questa notizia è la più recente" },
+            { titolo: "Seconda notizia più recente", testo: "Questa notizia è meno recente della prima" },
+            { titolo: "Notizia meno recente", testo: "Questa è la notizia meno recente delle 3" }
+        ];
+
+        service.getNotizie = function () {
+            return notizie;
+        }
+    }
+    
+    function ChisiamoService() {
+        var service = this;
+        var experiments = [
+            {name: 'Experiment 1', description: 'This is an experiment', completed: 0},
+            {name: 'Experiment 2', description: 'This is an experiment', completed: 0}
+        ];
 
         service.getExperiments = function() {
             return experiments;
         };
-    })
-    .directive('experiment', function(){
-        var linker = function (scope, element, attrs) {
-            element.on('click', function(){
-                scope.doExperiment();
-            })
-        };
+    }
 
-        var controller =  function($scope){
-            $scope.doExperiment = function() {
-                $scope.$apply(function(){
-                    $scope.experiment.completed++;
-                });
-            };
-        };
 
-        return {
-            scope: true,
-            restrict: 'E',
-            template: '<div class="experiment">' +
-                '<h3>{{experiment.name}}</h3>' +
-                '<p>{{experiment.description}}</p>' +
-                '<p><strong>{{experiment.completed}}</strong></p>' +
-                '</div>',
-            link: linker,
-            controller: controller
+    function ProdottiService() {
+        var service = this;
+        var prodotti = [
+            { nome: "Nome p1", descrizione: "breve descrizione p1", imgSrc: "images/olio1.jpeg" },
+            { nome: "Nome p2", descrizione: "breve descrizione p2", imgSrc: "images/olio1.jpeg" },
+            { nome: "Nome p3", descrizione: "breve descrizione p3", imgSrc: "images/olio1.jpeg" },
+            { nome: "Nome p4", descrizione: "breve descrizione p4", imgSrc: "images/olio1.jpeg" }
+        ];
+
+        service.getProdotti = function() {
+            return prodotti;
+        };
+    }
+
+    function FooterService() {
+        var service = this;
+        var footers = [
+            { titolo: "Dove siamo", testo: "indirizzo" },
+            { titolo: "Contattaci", testo: "Recapito email" },
+            { titolo: "Dettagli azienda", testo: "Nome azienda e P. Iva" },
+            { titolo: "Seguici sui social network", testo: "Pagina fb, twitter, ..."}
+        ];
+
+        service.getFooters = function() {
+            return footers;
         }
-    });
+    }
+
+})();
